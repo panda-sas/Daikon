c467f4ce79ad536e0e7d7ed602e3fcd45bd48a2e
Saswati Panda <panda.sas20@gmail.com>
2024-01-30 12:04:43 -0600

    events of Building Blocks

--- src/Services/Comment/Comment.Domain/Entities/CommentReply.cs
+++ src/Services/Comment/Comment.Domain/Entities/CommentReply.cs
@@ -5,6 +5,7 @@ namespace Comment.Domain.Entities
     public class CommentReply : BaseEntity
     {
         public Guid CommentId { get; set; }
+        public Guid ResourceId { get; set; }
         public DVariable<string>? Body { get; set; }
         public string? PostedBy { get; set; }
         
