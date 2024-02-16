f62f57bbfa10f418a10a42157152370016d34705
Saswati Panda <panda.sas20@gmail.com>
2024-02-16 11:12:07 -0600

    comments

--- src/Services/Comment/Comment.Application/ApplicationServiceRegistration.cs
+++ src/Services/Comment/Comment.Application/ApplicationServiceRegistration.cs
@@ -1,10 +1,27 @@
-﻿using FluentValidation;
+﻿using Comment.Application.EventHandlers;
+using Comment.Application.Features.Commands.NewComment;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Comment.Application.Mappings;
 
 namespace Comment.Application;
 
-public class Class1
-{
+public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
 
-}
+            services.AddAutoMapper(typeof(MappingProfile).Assembly);
+            services.AddMediatR(typeof(NewCommentCommand).Assembly);
+            services.AddValidatorsFromAssembly(typeof(NewCommentCommandValidator).Assembly);
+
+            // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(Behaviours.UnhandledExceptionBehaviour<,>));
+            // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(Behaviours.ValidationBehaviour<,>));
+
+            services.AddScoped<ICommentEventHandler, CommentEventHandler>();
+
+            return services;
+        }
+
+    }
--- src/Services/Comment/Comment.Application/Features/Queries/GetComment/CommentVM.cs
+++ src/Services/Comment/Comment.Application/Features/Queries/GetComment/CommentVM.cs
@@ -8,8 +8,8 @@ namespace Comment.Application.Features.Queries.GetComment
         public Guid ResourceId { get; set; }
         public string? Reference { get; set; }
         public string? Section { get; set; }
-        public DVariable<string> Topic { get; set; }
-        public DVariable<string>? Description { get; set; }
+        public object Topic { get; set; }
+        public object Description { get; set; }
         public string? PostedBy { get; set; }
         
     }
